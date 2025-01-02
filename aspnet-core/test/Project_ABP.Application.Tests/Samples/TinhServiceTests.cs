95487f6cb37d6eb9e60d6d76d3da617c6b3637de
PhamQuangVinh77 <quangvinh770808@gmail.com>
2025-01-03 04:46:30 +0700

    update integration test

--- aspnet-core/test/Project_ABP.Application.Tests/Samples/TinhServiceTests.cs
+++ aspnet-core/test/Project_ABP.Application.Tests/Samples/TinhServiceTests.cs
@@ -1,30 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Project_ABP.Dto.TinhDto;
 using Project_ABP.Entities;
 using Project_ABP.Filter;
 using Project_ABP.IRepositories;
-using Project_ABP.IServices;
 using Project_ABP.Services;
-using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Volo.Abp.Domain.Repositories;
-using Volo.Abp.Modularity;
 using Xunit;
 
 namespace Project_ABP.Samples
 {
-    public class TinhServiceTests<TStartupModule> : Project_ABPApplicationTestBase<TStartupModule> where TStartupModule : IAbpModule
+    public class TinhServiceTests
     {
         private readonly Mock<IRepository<Tinh, Guid>> _repository;
         private readonly Mock<ITinhRepository> _tinhRepository;
         private readonly Mock<ILogger<TinhService>> _logger;
         private readonly IMapper _mapper;
         private readonly TinhService _tinhService;
-        private readonly ITinhService _tinhServiceTest;
         public TinhServiceTests()
         {
             _repository = new Mock<IRepository<Tinh, Guid>>();
@@ -34,24 +30,21 @@ namespace Project_ABP.Samples
             _mapper = mockMapper.CreateMapper();
 
             _tinhService = new TinhService(_mapper, _repository.Object, _tinhRepository.Object, _logger.Object);
-            _tinhServiceTest = GetRequiredService<ITinhService>();
         }
 
         [Fact]
         public async Task GetAllTinhs_ValidQuery_SuccessResult()
         {
-            //var listTinh = new List<Tinh>
-            //{
-            //    new Tinh { MaTinh = 30, TenTinh = "Hà Nội", Cap = "Thành phố trung ương"},
-            //    new Tinh { MaTinh = 34, TenTinh = "Hải Dương", Cap = "Tỉnh"},
-            //    new Tinh { MaTinh = 89, TenTinh = "Hưng Yên", Cap = "Tỉnh"},
-            //    new Tinh { MaTinh = 17, TenTinh = "Thái Bình", Cap = "Tỉnh"},
-            //};
-            //_tinhRepository.Setup(x => x.GetAllTinhs()).ReturnsAsync(listTinh);
-            //var result = await _tinhService.GetAllTinhs();
-            //Assert.Equal(listTinh.Count, result.Count);
-            var result = await _tinhServiceTest.GetListAsync(new TinhPagedAndSortedResultRequestDto());
-            result.TotalCount.ShouldBeGreaterThan(0);
+            var listTinh = new List<Tinh>
+            {
+                new Tinh { MaTinh = 30, TenTinh = "Hà Nội", Cap = "Thành phố trung ương"},
+                new Tinh { MaTinh = 34, TenTinh = "Hải Dương", Cap = "Tỉnh"},
+                new Tinh { MaTinh = 89, TenTinh = "Hưng Yên", Cap = "Tỉnh"},
+                new Tinh { MaTinh = 17, TenTinh = "Thái Bình", Cap = "Tỉnh"},
+            };
+            _tinhRepository.Setup(x => x.GetAllTinhs()).ReturnsAsync(listTinh);
+            var result = await _tinhService.GetAllTinhs();
+            Assert.Equal(listTinh.Count, result.Count);
         }
 
         [Fact]
--- aspnet-core/test/Project_ABP.TestBase/Project_ABPTestDataSeedContributor.cs
+++ aspnet-core/test/Project_ABP.TestBase/Project_ABPTestDataSeedContributor.cs
@@ -1,41 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
-using Project_ABP.Entities;
-using Project_ABP.IRepositories;
+﻿using System.Threading.Tasks;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
-using Volo.Abp.Domain.Repositories;
 
 namespace Project_ABP;
 
 public class Project_ABPTestDataSeedContributor : IDataSeedContributor, ITransientDependency
 {
-    private readonly IRepository<Tinh, Guid> _tinhRepository;
-    public Project_ABPTestDataSeedContributor(IRepository<Tinh, Guid> tinhRepository)
-    {
-        _tinhRepository = tinhRepository;
-    }
     public async Task SeedAsync(DataSeedContext context)
     {
-        await _tinhRepository.InsertAsync(new Tinh
-        {
-            MaTinh = 30,
-            TenTinh = "Hà Nội",
-            Cap = "Thành phố trung ương"
-        });
-
-        await _tinhRepository.InsertAsync(new Tinh
-        {
-            MaTinh = 34,
-            TenTinh = "Hải Dương",
-            Cap = "Tỉnh"
-        });
-
-        await _tinhRepository.InsertAsync(new Tinh
-        {
-            MaTinh = 89,
-            TenTinh = "Hưng Yên",
-            Cap = "Tỉnh"
-        });
+        await Task.CompletedTask;
     }
 }
